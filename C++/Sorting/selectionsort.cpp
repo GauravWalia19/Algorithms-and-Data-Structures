c253d7a1771e242555cc514f1524cbc195e7c1b0
Gaurav Walia <gauravwalia019@gmail.com>
2018-06-13 17:21:27 +0530

    updated wrong selection sort

--- C++/Sorting/selectionsort.cpp
+++ C++/Sorting/selectionsort.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
- 
 using namespace std;
- 
-// Sort arr[] of size n using Selection Sort. 
-void SelectionSort (int arr[], int n)
-{
-	int i, j;
-	for (i = 0; i < n; ++i)
-	{
-		for (j = i+1; j < n; ++j)
-		{
-			// Comparing consecutive data and switching values if value at i > j.
-			if (arr[i] > arr[j])
-			{
-				arr[i] = arr[i]+arr[j];
-				arr[j] = arr[i]-arr[j];
-				arr[i] = arr[i]-arr[j];
-			}	
-		}
-		// Value at i will be minimum of all the values above this index.
-	}	
-}	
- 
+void SelectionSort(int*,int);
 int main()
 {
-	int n, i;
-	cout<<"\nEnter the number of data element to be sorted: ";
-	cin>>n;
- 
+	int n,i;
+	cout << "Enter the number of data element to be sorted: ";
+	cin >> n;
 	int arr[n];
+	cout << "Enter the elements of the array" << endl;
 	for(i = 0; i < n; i++)
 	{
-		cout<<"Enter element "<<i+1<<": ";
 		cin>>arr[i];
 	}
- 
-	SelectionSort(arr, n);
- 
+	/*
+	//selection sort
+	
+	*/
+	SelectionSort(arr,n);
 	// Display the sorted data.
 	cout<<"\nSorted Data ";
 	for (i = 0; i < n; i++)
-		cout<<"->"<<arr[i];
- 
+		cout << arr[i] << " ";
+	cout << endl;
 	return 0;
+}
+void SelectionSort(int array[],int s)
+{
+	int max; //maximum index
+	for(int i=s-1;i>0;i--)
+	{
+		max=0;
+		//finding max element index
+		for(int j=0;j<=i;j++)
+		{
+			if(array[max]<array[j])
+			{
+				max = j;
+			}
+		}
+		//swapp
+		int temp = array[i];
+		array[i] = array[max];
+		array[max] = temp;
+	}
 }
