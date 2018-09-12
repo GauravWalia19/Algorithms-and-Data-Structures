26761805575bd88ec19c4d8d2a4aa43d3ba55f25
GauravWalia19 <gauravwalia019@gmail.com>
2018-09-12 14:46:24 +0530

    updated the fibonacci number support:TOO MUCH BUGS IN THIS CODE SOLVED

--- C/Misc/fibonacci.c
+++ C/Misc/fibonacci.c
@@ -1,28 +1,34 @@
 /* FIBONACCI SERIES
  * 
  * 0 1 1 2 3 5 8 13 .....
- * VALID UPTO 93 terms
+ * 
  */
 
 #include <stdio.h>
-
-int fibonacci(int M) 
+void fibonacci(int M) 
 {
-    long long a=0,b=1,i,c;
-    printf("Fibonacci series is:\n%lld\n%lld",a,b);
-    //for loop to find all fibonnaci number
-    for (i=0;i<M-2;i++) 
+    unsigned long long a=0,b=1,i,c;
+    if(M==1)
     {
-        c=a+b;//fibonacci series is addition of previous two numbers
-        a=b;
-        b=c;
-        printf("%lld ",c);
+        printf("Fibonacci series is:\n%lld",a);
+    }
+    else
+    {
+        printf("Fibonacci series is:\n%llu %llu ",a,b);
+        //for loop to find all fibonnaci number
+        for (i=0;i<M-2;i++) 
+        {
+            c=a+b;//fibonacci series is addition of previous two numbers
+            a=b;
+            b=c;
+            printf("%llu ",c);
+        }
     }
     printf("\n");
-    return 0;
 }
 
-int main() {
+int main()
+{
     int N;
     printf("Enter the no of terms: ");
     scanf("%d",&N);
